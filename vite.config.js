614988bd7a0eb157eabdda679012bc01d424c3d8
freisterortizguevara-wq <freisterortizguevara@gmail.com>
2026-08-06 16:56:34 -0500

    🔥 Configurando rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ✅ Cambiado a ruta relativa
+  base: '/Terpel_Ptecnica/',
   server: {
     port: 3000,
   }
